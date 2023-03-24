01a8b7f7037ba33a436112d76c47c8f0f8d001e4
Pedro Ivan <pedroivanlarios@gmail.com>
2023-03-24 11:00:47 -0600

    second commit

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener("install", e =>{
 self.addEventListener("fetch", e  =>{
     e.respondWith(
         caches.match(e.request).then(response => {
-            return response || fech(e.request);
+            return response || fetch(e.request);
         })
     );
 });
